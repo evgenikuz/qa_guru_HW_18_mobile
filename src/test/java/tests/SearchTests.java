e61a72697ae57cd5417b28d9c0ceaf1b5a59a3d7
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-09-11 01:39:32 +0300

    deleted unused imports

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,11 +1,9 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
